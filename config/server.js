316a21137479a28f98fddbe80a939c1fdeacd1bf
Reza <Reza.Rahmati@gmail.com>
2021-11-28 18:46:55 -0500

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '61ea2359db9ac1c8b21538ff33676f26'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2173ed38-8008-46b9-aa47-b4f8b6841a20'
+};
